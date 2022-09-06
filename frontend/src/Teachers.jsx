b91f9ee0843f781f6984297f4d1b16338db3a761
Steven-Otieno <steveotianga8991@gmail.com>
2022-09-06 16:04:26 +0300

    Added the Teachers sample Table

--- frontend/src/Teachers.jsx
+++ frontend/src/Teachers.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Teachers = () => {
   return (
-    <div>Teachers</div>
+    <div className="container" style={{marginTop:'150px'}}>
+      <table className="table">
+        <thead className="table-dark">
+          <tr>
+            <th>ID NO:</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
