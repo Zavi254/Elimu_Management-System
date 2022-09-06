f29ef5e4a4940aff4f2c73c82a034081940cc1a2
Steven-Otieno <steveotianga8991@gmail.com>
2022-09-06 13:34:10 +0300

    imported the Department, Student, Teachers and Dashboard component and insert Route links in the App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
-import Navbar from './Navbar';
+import React from "react";
+import "./App.css";
+import Department from "./Department";
+import Navbar from "./Navbar";
+import Student from "./Student";
+import Teachers from "./Teachers";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
-    <div className='container-fluid'>
-      <Navbar />
+    <div className="d-flex justify-content-between">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="students" element={<Student />} />
+          <Route path="teachers" element={<Teachers />} />
+          <Route path="department" element={<Department />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
