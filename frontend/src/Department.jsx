252d1c80b68eda93c43f2f79f11244e106a9dd11
Steven-Otieno <steveotianga8991@gmail.com>
2022-09-06 16:03:19 +0300

    Added the Department sample Table

--- frontend/src/Department.jsx
+++ frontend/src/Department.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Department = () => {
   return (
-    <div>Department</div>
+    <div className="container" style={{marginTop:'150px'}}>
+      <table className="table">
+        <thead className="table-dark">
+          <tr>
+            <th>ID NO:</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
