031e090161853881c4e7e6fa0842eac380b9e0e7
Steven-Otieno <steveotianga8991@gmail.com>
2022-09-06 16:03:54 +0300

    Added the Student sample Table

--- frontend/src/Student.jsx
+++ frontend/src/Student.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import DeleteButton from "./DeleteButton";
+import UpdateButton from "./UpdateButton";
 
 const Student = () => {
   return (
-    <div>
-      Student
+    <div className="container" style={{marginTop:'150px'}}>
+      <table className="table">
+        <thead className="table-dark">
+          <tr>
+            <th>ID NO:</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>DOB</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>22</td>
+            <td>Male</td>
+            <th>12/03/2000</th>
+            <td><UpdateButton /></td>
+            <td><DeleteButton /></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Student
+export default Student;
