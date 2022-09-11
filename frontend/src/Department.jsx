ad7bf9b3aadbfb74f5a716cbfa5dff7b954fc99c
Steven-Otieno <steveotianga8991@gmail.com>
2022-09-11 11:02:52 +0300

    Fetched the API endpoint for the departments API

--- frontend/src/Department.jsx
+++ frontend/src/Department.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 const Department = () => {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/departments')
+      .then(res => res.json())
+      .then(data => setData(data))
+  },[])
+
   return (
     <div className="container" style={{marginTop:'150px'}}>
       <table className="table bg-white" style={{fontSize:'1.3rem'}}>
@@ -11,10 +22,14 @@ const Department = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-          </tr>
+          {data.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.name}</td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </div>
